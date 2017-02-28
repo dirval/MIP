42f15ae3cbf6a564f6e6adad3ebce11a64c4512d
dirval <dirval@debian>
2017-02-28 16:33:02 +0100

    make better

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ app.post('/upload', parser.single('image'), function (req, res) {
       url_image = req.file.url;
       console.log(url_image);
     }
-    if (req.body != "") {
+    if (req.body !== 0) {
       console.log(req.body);
       console.log(req.body.id_user);
       console.log(req.body.description);
